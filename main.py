ca3a962b6c2dec9b2b64c5267975eab4c4681845
Setofnosets <locuartu@gmail.com>
2022-11-20 19:50:41 -0600

    Agregado longitud de onda

--- main.py
+++ main.py
@@ -4,21 +4,27 @@ from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 from matplotlib.pyplot import figure
 import numpy as np
 
-def sine_wave(A: float, f: float, phi: float, t:float) -> float:
-    return A*sin((2*pi*t*f) + phi)
+
+def sine_wave(A: float, p: float, phi: float, t: float) -> float:
+    return A * sin((2 * pi * t) / p + phi)
+
 
 #Variable a modificar
-fase = 3*pi/2
+longitud = 3.
 
-
-
-amp_max = 0.
-x_max = 2*pi
+amp_half = 0.
+x_max = 2 * pi
 x_min = 0
 step = 0.01
+
+
 x = np.arange(x_min, x_max, step)
-y = [sine_wave(1, 1, fase, tiempo) for tiempo in x]
-amp_max = y.index(max(y)) * step
+y = [sine_wave(1, longitud, 0, tiempo) for tiempo in x]
+
+for i in range(len(y)):
+    if abs(y[i] - 0.5) < step:
+        amp_half = i * step
+        break
 
 #Agregar Ejes
 ax = plt.figure().add_subplot(1, 1, 1)
@@ -29,11 +35,11 @@ ax.yaxis.set_major_formatter(FormatStrFormatter('%g'))
 ax.yaxis.set_ticks(np.arange(-100, 100, 0.5))
 ax.xaxis.set_minor_locator(MultipleLocator(0.1))
 ax.yaxis.set_minor_locator(MultipleLocator(0.1))
-ax.xaxis.grid(True,'minor',linewidth=0.5)
-ax.yaxis.grid(True,'minor',linewidth=0.5)
+ax.xaxis.grid(True, 'minor', linewidth=0.5)
+ax.yaxis.grid(True, 'minor', linewidth=0.5)
 plt.grid(True, linewidth=0.5, color='#e5e5e5', linestyle='-')
-plt.annotate("Fase: " + str(fase), xy=(-0.2,1.1), xytext=(-0.2,1.1))
-
+plt.annotate("", xy=(amp_half, 0.5), xytext=(amp_half + longitud, 0.5), arrowprops=dict(arrowstyle='|-|'))
+plt.annotate("λ: " + str(longitud), xy=(amp_half + longitud / 2, 0.6), xytext=(amp_half + longitud / 2, 0.6))
 #Mostrar Grafica
-plt.plot(x,y)
+plt.plot(x, y)
 plt.show()
