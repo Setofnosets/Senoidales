3fbdfa724e5b870f046c2b522fa408feea8ee502
Setofnosets <locuartu@gmail.com>
2022-11-20 21:45:15 -0600

    Agregado Modulacion por amplitud

--- main.py
+++ main.py
@@ -1,45 +1,59 @@
-from math import sin, pi
+from math import sin, pi, cos
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 from matplotlib.pyplot import figure
 import numpy as np
 
+#%matplotlib inline
 
-def sine_wave(A: float, p: float, phi: float, t: float) -> float:
-    return A * sin((2 * pi * t) / p + phi)
+def sine_wave(A: float, f: float, phi: float, t:float) -> float:
+    return A*sin((2*pi*t*f) + phi)
+
+def mensaje(M: float, f: float, phi: float, t: float) -> float:
+    return M*cos((2*pi*t*f) + phi)
+
+def modulacion_amplitud(A: float, M: float, fc: float, fm: float, phi: float, t: float) -> float:
+    indice = M/A
+    return (1 + mensaje(indice, fm, phi, t))*sine_wave(A, fc, phi, t)
+def mostrar_grafica(x: list, y: list, title: str, fig: any, y1: list=None, y2: list=None) -> None:
+    fig.suptitle(title, fontsize=16)
+
+    ax = fig.add_axes([0,0,1,1])
+    #ax.set_title(title)
+    ax.spines['bottom'].set_position('center')
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%g'))
+    ax.yaxis.set_ticks(np.arange(-100, 100, 0.5))
+    ax.xaxis.set_minor_locator(MultipleLocator(0.1))
+    ax.yaxis.set_minor_locator(MultipleLocator(0.1))
+    ax.xaxis.grid(True, 'minor', linewidth=0.5)
+    ax.yaxis.grid(True, 'minor', linewidth=0.5)
+    ax.plot(x, y, 'g')
+    if y1 is not None:
+        ax.plot(x, y1, 'r')
+    if y2 is not None:
+        ax.plot(x, y2, 'b')
 
 
-#Variable a modificar
-longitud = 3.
-
-amp_half = 0.
-x_max = 2 * pi
+amp_max = 0.
+x_max = 2*pi
 x_min = 0
 step = 0.01
-
-
 x = np.arange(x_min, x_max, step)
-y = [sine_wave(1, longitud, 0, tiempo) for tiempo in x]
+#Señal con A = 3, f = 0.5 y fase = 0
+mensaje1 = [mensaje(3,0.5,0,tiempo) for tiempo in x]
+#Señal con A = 1, f = 5 y fase = 0
+transmision = [sine_wave(10,20,0,tiempo) for tiempo in x]
+#Señal modulada
+modulada = [modulacion_amplitud(1,3,20,0.5,0,tiempo) for tiempo in x]
 
-for i in range(len(y)):
-    if abs(y[i] - 0.5) < step:
-        amp_half = i * step
-        break
+fig = plt.figure()
+mostrar_grafica(x, mensaje1, "Mensaje", fig)
+fig = plt.figure()
+mostrar_grafica(x, transmision, "Señal de transmision", fig)
 
-#Agregar Ejes
-ax = plt.figure().add_subplot(1, 1, 1)
-ax.spines['bottom'].set_position('center')
-ax.spines['right'].set_color('none')
-ax.spines['top'].set_color('none')
-ax.yaxis.set_major_formatter(FormatStrFormatter('%g'))
-ax.yaxis.set_ticks(np.arange(-100, 100, 0.5))
-ax.xaxis.set_minor_locator(MultipleLocator(0.1))
-ax.yaxis.set_minor_locator(MultipleLocator(0.1))
-ax.xaxis.grid(True, 'minor', linewidth=0.5)
-ax.yaxis.grid(True, 'minor', linewidth=0.5)
-plt.grid(True, linewidth=0.5, color='#e5e5e5', linestyle='-')
-plt.annotate("", xy=(amp_half, 0.5), xytext=(amp_half + longitud, 0.5), arrowprops=dict(arrowstyle='|-|'))
-plt.annotate("λ: " + str(longitud), xy=(amp_half + longitud / 2, 0.6), xytext=(amp_half + longitud / 2, 0.6))
-#Mostrar Grafica
-plt.plot(x, y)
+#Mostrar Graficas moduladas
+fig = plt.figure()
+mostrar_grafica(x, modulada, "Señal modulada", fig)
 plt.show()
